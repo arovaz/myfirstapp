5aad8bea1b3dcc50eaeb4f8db4893443d6a39f5a
Arthur Vaz <arovaz2013@gmail.com>
2023-11-30 23:53:11 -0300

    site grandão

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+var global = 2;
 function submitForm() {
     var name = document.getElementById('name').value;
     var phone = document.getElementById('phone').value;
-  
-    var outputText = "Nome: " + name + "<br>Telefone: " + phone;
-  
+    global = global + 1;
+    
+    var teste1 = "S";
+    var teste2 = 2;
+    var resultado = teste1 + teste2;
+    var loucura = arthurSomaNumeros(15, 9, 23);
+    var outputText = "Nome: " + name + "<br>Telefone: " + phone + resultado + loucura;
     document.getElementById('outputText').innerHTML = outputText;
     document.getElementById('outputBox').classList.remove('hidden');
+    console.log("O pai tá on!");
+
   }
   
+  function arthurSomaNumeros (a,b,c){
+    global = global + 1;
+    var resultado = a + b + c;
+    return resultado;
+  }
