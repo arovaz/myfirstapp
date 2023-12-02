6f3d2787ccac2ba0c9eb94d5d3c6be85af49b45c
Arthur Vaz <arovaz2013@gmail.com>
2023-12-02 16:51:25 -0300

    Incerssão de menu lateral

--- inicial.js
+++ inicial.js
@@ -1,3 +1,6 @@
 document.getElementById('logoffBtn').addEventListener('click', function() {
     window.location.href = 'index.html';
 });
+function showContent(contentType) {
+    document.getElementById('content').innerText = `Conteúdo relacionado a ${contentType} vai aqui.`;
+}
