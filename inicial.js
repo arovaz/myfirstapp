a1284dcdc2a40891bdb7ab6ca7ecd6bb9e7c7853
Arthur Vaz <arovaz2013@gmail.com>
2023-12-02 18:18:08 -0300

    3 pedidos

--- inicial.js
+++ inicial.js
@@ -4,3 +4,29 @@ document.getElementById('logoffBtn').addEventListener('click', function() {
 function showContent(contentType) {
     document.getElementById('content').innerText = `Conteúdo relacionado a ${contentType} vai aqui.`;
 }
+function showContent(contentType) {
+    const contentElement = document.getElementById('content');
+    
+    switch (contentType) {
+        case 'Locacao':
+        case 'Manutencao':
+        case 'Documentos':
+        case 'Compras':
+            contentElement.innerHTML = `
+                <h2>${contentType}</h2>
+                <ol>
+                    <li onclick="showSubContent('Novo pedido')">Novo pedido</li>
+                    <li onclick="showSubContent('Status de pedido')">Status de pedido</li>
+                    <li onclick="showSubContent('Histórico de pedido')">Histórico de pedido</li>
+                </ol>`;
+            break;
+        default:
+            contentElement.innerText = `Conteúdo relacionado a ${contentType} vai aqui.`;
+            break;
+    }
+}
+
+function showSubContent(subContentType) {
+    const contentElement = document.getElementById('content');
+    contentElement.innerHTML = `<p>${subContentType} vai aqui.</p>`;
+}
