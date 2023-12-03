1a6cef2c5fb205d6dfb0dcc6e3101d54bdd08229
Arthur Vaz <arovaz2013@gmail.com>
2023-12-03 00:17:06 -0300

    Coleta de dados para manutenção

--- inicial.js
+++ inicial.js
@@ -41,3 +41,41 @@
         const submenuElement = document.getElementById('submenu');
         submenuElement.style.display = 'none';
     }
+
+
+    //Parte da coleta de dados para Manutenção
+    function showMaintenanceForm() {
+        document.getElementById('content').style.display = 'none';
+        document.getElementById('maintenanceForm').style.display = 'block';
+        document.getElementById('submenu').style.display = 'none';
+    }
+    
+    function submitMaintenanceForm() {
+        // Aqui você pode coletar os valores do formulário e fazer o que for necessário
+        const project = document.getElementById('project').value;
+        const maintenanceType = document.getElementById('maintenanceType').value;
+        const plate = document.getElementById('plate').value;
+        const requester = document.getElementById('requester').value;
+        const driver = document.getElementById('driver').value;
+        const currentKm = document.getElementById('currentKm').value;
+        const vehicleLocation = document.getElementById('vehicleLocation').value;
+        const maintenanceDate = document.getElementById('maintenanceDate').value;
+    
+        // Exemplo: exibir os valores no console
+        console.log('Projeto:', project);
+        console.log('Tipo de Manutenção:', maintenanceType);
+        console.log('Placa:', plate);
+        console.log('Solicitante:', requester);
+        console.log('Condutor:', driver);
+        console.log('Km Atual:', currentKm);
+        console.log('Local do Veículo:', vehicleLocation);
+        console.log('Data para a Manutenção:', maintenanceDate);
+    
+        // Aqui você pode realizar outras ações, como enviar os dados para um servidor, etc.
+    
+        // Voltar ao conteúdo principal
+        document.getElementById('content').style.display = 'block';
+        document.getElementById('maintenanceForm').style.display = 'none';
+        document.getElementById('submenu').style.display = 'block';
+    }
+    
